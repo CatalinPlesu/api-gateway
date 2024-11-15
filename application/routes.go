4271dd756f15f3031e48a9389ff69551cf2e9736
Catalin Plesu <catalinhimself@gmail.com>
2024-11-15 11:35:41 +0200

    chore(init)

---
+++ application/routes.go
@@ -0,0 +1,41 @@
+package application
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/CatalinPlesu/user-service/handler"
+	"github.com/CatalinPlesu/user-service/repository/user"
+)
+
+func (a *App) loadRoutes() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	router.Route("/users", a.loadUserRoutes)
+
+	a.router = router
+}
+
+func (a *App) loadUserRoutes(router chi.Router) {
+	userHandler := &handler.User{
+		Repo: &user.RedisRepo{
+			Client: a.rdb,
+		},
+	}
+
+	router.Post("/", userHandler.Create)                                   
+	router.Get("/", userHandler.List)                                      
+	// router.Get("/username/{username}", userHandler.GetByUsername)          
+	// router.Get("/displayname/{displayname}", userHandler.GetByDisplayName) 
+	router.Get("/{id}", userHandler.GetByID)                               
+	router.Put("/{id}", userHandler.UpdateByID)                            
+	router.Delete("/{id}", userHandler.DeleteByID)                         
+}
